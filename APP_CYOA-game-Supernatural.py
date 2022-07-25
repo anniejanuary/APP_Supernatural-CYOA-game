9e5de41c4452415d4ca216418e613a9e3a735965
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-25 12:20:20 +0200

    Update APP_CYOA-game-Supernatural.py

--- APP_CYOA-game-Supernatural.py
+++ APP_CYOA-game-Supernatural.py
@@ -27,13 +27,13 @@ print('''
 
 print("Welcome to Supernatural, Dean! \nIt's the top simulator for killing monsters, hunting things, the family business! \nYour goal is to not go to hell or become a demon.")
 
-sell_soul= input("\nYour brother Sam is dead. He will be resurrected by Death if you sell your soul to be reaped in a year. Do you sell your soul? Yes/No: \n")
-if sell_soul == "yes" or sell_soul == "Yes" or sell_soul == "YES":
+sell_soul= input("\nYour brother Sam is dead. He will be resurrected by Death if you sell your soul to be reaped in a year. Do you sell your soul? Yes/No: \n").lower()
+if sell_soul == "yes":
   print("You sold your soul and Sam is back. But after a year of trying to find a way out, the crossroads demon Crowley     sics a hellhound on you. You die and are dragged to hell. Game over. No more pie for you.")
 else:
-  purgatory= input("You don't sell your soul, but find a different way to bring Sam back (as always). You kill the Leviathans' leader (the new baddie) and get dragged into Purgatory. Purgatory vampires surround you. Do you fight or wait? \n")
-  if purgatory == "fight" or purgatory == "Fight" or purgatory == "FIGHT":
-    door = input("You kill the vamps. To escape Purgatory you need to choose the right door: red, white or black: \n")
+  purgatory= input("You don't sell your soul, but find a different way to bring Sam back (as always). You kill the Leviathans' leader (the new baddie) and get dragged into Purgatory. Purgatory vampires surround you. Do you fight or wait? \n").lower()
+  if purgatory == "fight":
+    door = input("You kill the vamps. To escape Purgatory you need to choose the right door: red, white or black: \n").lower()
     if door == "red":
       print("You keep living your life hunting monsters with your brother Sam happily ever after and having unlimited pie. You win. Sweet!")
     elif door == "white":
@@ -42,5 +42,5 @@ else:
       print(f"You escape Purgatory but are killed by angel Metatron (there are angels now?!) with an angel blade (sure, there might as well be angel blades now) and die (shocker). Crowley, the prince of Hell, resurrects you as a demon (yay?). Oh wait, nay! Game over.")
     else:
       print("Enter: red, white or black: \n")
-  elif purgatory == "wait" or purgatory == "Wait" or purgatory == "WAIT":
+  else:
     print("You run away from the Purgatory vamps but are stuck in Purgatory for eternity. No pie in Purgatory. Game over!")
